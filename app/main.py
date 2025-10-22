93cee4e7ead21a29a71876cab8d488ceff7b2173
Danco <dancornioo@gmail.com>
2025-10-22 19:34:29 -0300

    Add initial project files including .gitignore, main.py, and requirements.txt

---
+++ app/main.py
@@ -0,0 +1,20 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+url = 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv'
+food_consumption = pd.read_csv(url)
+
+rice_consumption = food_consumption[food_consumption['food_category'] == 'Rice']
+
+
+# plt.hist(rice_consumption['co2_emmission'], color='red')
+# plt.show()
+
+rice_consumption['co2_emmission'].hist(color='green')
+plt.show()
+
+rice_consumption['consumption'].hist(color='red')
+plt.show()
